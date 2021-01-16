7b7a1ac714737cafafe7ea400f25dac634c6a99a
root <root@ip-10-0-1-20.cn-northwest-1.compute.internal>
2021-01-16 08:36:16 +0000

    test15

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,6 +19,7 @@ login.login_view = 'login'
 
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 print('who used me:',__name__)
 
