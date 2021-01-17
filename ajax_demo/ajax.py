99d1fc212ea448e87e82f2bc44345de833f00d35
root <root@ip-10-0-1-20.cn-northwest-1.compute.internal>
2021-01-17 08:31:28 +0000

    test18

--- ajax_demo/ajax.py
+++ ajax_demo/ajax.py
@@ -1,17 +1,27 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('user.html')
 
 @app.route('/random')
 def genbum():
     maxnum = request.args.get('max')
     value = random.randint(0, int(maxnum))
     return str(value)
+
+@app.route('/checkUser',methods=['get','post'])
+def checkUser():
+    usernames = ['lewis','lucy']
+    username =request.form.get('username')
+    res = {'flag':0} #用户不存在可以注册
+    if username in usernames:
+        res['flag'] = 1
+
+    return jsonify(res)
 
 
 if __name__ == '__main__':
